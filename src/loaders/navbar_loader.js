fea957eb0397049b30f70d2aea05c91b992ad8d4
JAG008 <jag008@lvc.edu>
2024-11-11 12:44:23 -0500

    Created dynamic navbar

---
+++ src/loaders/navbar_loader.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("src/elements/navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector(".navbar").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+});
