616ec8f79caf94e1c318e559f31125f93928f291
Joseph Goss <josephgoss123@gmail.com>
2024-11-21 14:56:42 -0500

    temporary

---
+++ src/loaders/mobile_menu_loader.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("src/elements/mobile_menu.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector(".mobile-menu").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+});
